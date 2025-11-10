cce111263c6414a73283395b000aa825c3f9265c
Sahil Paul <paulcode1234@gmail.com>
2025-11-11 00:16:05 +0530

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router/dom";
+import router  from "./routing/router.js";
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <StrictMode>
+    <RouterProvider router={router} />,
+  </StrictMode>
+);
